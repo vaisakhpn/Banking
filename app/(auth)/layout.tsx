fc27efb80c65dbf49442c1e95cc45cdd23d88715
Vaisakh <vaisakhpn78@gmail.com>
2024-05-26 12:31:18 +0530

    routing

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,12 @@
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <main>
-        SIDEBAR
-        {children}
-      </main>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+      SIDEBAR
+      {children}
+    </main>
+  );
+}
